615c40ed269256f5bb0e52edcb6296b40f935ffd
浩楠 <@Haonan-Laptop>
2016-02-21 23:44:33 -0700

    github

---
+++ Assignment 1/src/cse360assign1/a.java
@@ -0,0 +1,16 @@
+package cse360assign1;
+public class a {
+public static void main(String[] args)
+{
+	OrderedIntList a = new OrderedIntList();
+	a.insert(3);
+	a.insert(5);
+	a.insert(1);
+	a.insert(8);
+	a.insert(11);
+	a.insert(18);
+	a.insert(1);
+	a.insert(19);
+	a.print();
+}
+}
