2fcdae4b4896a65810fafe352d6d8bcfe47a2978
浩楠 <@Haonan-Laptop>
2016-02-22 01:53:39 -0700

    assign 3

--- Assignment 1/src/cse360assign3/Calculator.java
+++ Assignment 1/src/cse360assign3/Calculator.java
@@ -1,34 +1,70 @@
 
 package cse360assign3;
-
+/**
+ *  Calculator class is compute the total value through add 
+ * subtract, multiply and divide
+ * @author Haonan Li for CSE360 Spring 2016
+ *
+ */
 public class Calculator {
-
+	/**
+	 * declare the parameter total
+	 * @param int total 
+	 */
 	private int total;
-	
+	/**
+	 * constructor set the total to 0
+	 * @param int total
+	 */
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
 	}
-	
+	/**
+	 * function using to get total
+	 * @return
+	 */
 	public int getTotal () {
 		return 0;
 	}
-	
+	/**
+	 * 
+	 * @param value
+	 */
 	public void add (int value) {
-		
+		total = total + value;
 	}
-	
+	/**
+	 * 
+	 * @param value
+	 */
 	public void subtract (int value) {
-		
+		total = total - value;
 	}
-	
+	/**
+	 * 
+	 * @param value
+	 */
 	public void multiply (int value) {
-		
+		total = total * value;
 	}
-	
+	/**
+	 * 
+	 * @param value
+	 */
 	public void divide (int value) {
-		
+		if (value == 0)
+		{
+			total = 0;
+		}
+		else
+		{
+			total = total / value;
+		}
 	}
-	
+	/**
+	 * 
+	 * @return
+	 */
 	public String getHistory () {
 		return "";
 	}
---
+++ Assignment 1/src/cse360assign3/testCalculator.java
@@ -0,0 +1,15 @@
+package cse360assign3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testCalculator {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+
+}
